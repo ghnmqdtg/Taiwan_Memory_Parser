586a46f784afd340bdee5e816b112a46ea81b5f3
ghnmqdtg <ghnmqdtg@gmail.com>
2020-11-06 12:39:38 +0800

    Feat: Add requirement.txt, update README.md and GUI appearance.

--- resource_parser.py
+++ resource_parser.py
@@ -15,7 +15,7 @@ random_user = [
     "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
     "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
     "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0"
 ]
 
 random_proxies = [
