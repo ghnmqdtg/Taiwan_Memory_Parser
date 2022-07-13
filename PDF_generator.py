5527f28fe1a12852f7e0eaa1aa9ced2140af7289
ghnmqdtg <ghnmqdtg@gmail.com>
2022-07-13 23:07:05 +0800

    Feat: Add folder selection feature for PDF exporting

--- PDF_generator.py
+++ PDF_generator.py
@@ -1,14 +1,29 @@
 import os
 from fpdf import FPDF
+from PyQt5 import (QtWidgets)
+
+
+def create_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 
 def img_to_pdf_convert(self):
-    if(self.output_path):
-        path = self.output_path
+    if not self.title:
+        path = QtWidgets.QFileDialog.getExistingDirectory(
+            self, "Choose the target folder", "./")
+        self.title = os.path.basename(path)
     else:
-        path = "./img/"
+        if self.output_path != '':
+            root = self.output_path
+        else:
+            root = "img"
 
-    path = f"./img/{self.title}"
+        path = f"./{root}/{self.title}"
+        if not os.path.exists(path):
+            return
+
+    create_path("./pdf")
 
     try:
         imagelist = [i for i in os.listdir(path) if i.endswith(".jpg")]
@@ -21,6 +36,10 @@ def img_to_pdf_convert(self):
         img_path = f"{path}/{image}"
         pdf.add_page()
         pdf.image(img_path, 0, 0, 210, 297)
+
+    # Remove the file if it exists
+    if os.path.exists(f"./pdf/{self.title}.pdf"):
+        os.remove(f"./pdf/{self.title}.pdf")
 
     # print("Picture loaded, Start Converting")
     pdf.output(f"./pdf/{self.title}.pdf", "F")
